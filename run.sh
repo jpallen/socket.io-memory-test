78a7c6028b8d6786b68fc841e12f91595f31e46d
James Allen <jamesallen0108@gmail.com>
2013-02-27 15:35:29 +0000

    Add results with patch

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-coffee --nodejs "--expose-gc" server.coffee --no-uploads | tee "just-websockets.csv"
-coffee --nodejs "--expose-gc" server.coffee --no-websockets | tee "just-uploads.csv"
-coffee --nodejs "--expose-gc" server.coffee | tee "websockets-and-uploads.csv"
+coffee --nodejs "--expose-gc" test.coffee --no-uploads | tee "just-websockets.csv"
+coffee --nodejs "--expose-gc" test.coffee --no-websockets | tee "just-uploads.csv"
+coffee --nodejs "--expose-gc" test.coffee | tee "websockets-and-uploads.csv"
 ./plot.sh
 
